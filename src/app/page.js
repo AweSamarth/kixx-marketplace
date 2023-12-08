6e6a1dd433a2ddbe7712d24b742882bb48a1008c
Harshit GAutam <harshitgtm02@gmail.com>
2023-12-09 00:42:11 +0530

    Footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,14 @@
 import Image from "next/image";
 import { Alert, Navbar, Dropdown, Avatar } from "flowbite-react";
 import NavbarNow from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import FooterNow from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
       <NavbarNow />
+      <FooterNow />
     </div>
   );
 }
