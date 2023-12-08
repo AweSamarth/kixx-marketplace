59e0c873a9176e152616dcddc3ff89f22f75d3f8
Divij <divijkathuria51714@gmail.com>
2023-12-09 01:17:41 +0530

    cards new

--- src/app/resell/page.js
+++ src/app/resell/page.js
@@ -1,17 +1,20 @@
-import ProjectCard from "../../../components/ProjectCard";
-
+import ProjectCard from "../../../components/Card";
 const getData = async()=>{
-    const res = await fetch('')
+    const res = await fetch(' "../../../dummy/data"')
     if(!res.ok){
         throw new Error("failed to fetch data")
     }
     return res.json
 }
 
-const resell = () => {
-    <div className='mt-20 flex flex-wrap gap-7'>
+export default async function resell(){
+    const projects = await getData()
+
+    return (
+        <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
+    )
 }
