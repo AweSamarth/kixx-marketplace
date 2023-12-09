1b4d3575dd0fd8bc57d2bc91d9886de49e6008b1
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-12-10 02:14:06 +0530

    ??

---
+++ src/app/testing/api/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
+import { ethers } from "ethers";
+
+
+const provider = new ethers.getDefaultProvider()
+
+const privateKey= process.env.PRIVATE_KEY
+
+let walletWithProvider = new ethers.Wallet(privateKey, provider); 
+
+
+export async function POST(req, res) {
+
+  const userAlice = await PushAPI.initialize(walletWithProvider, { env: CONSTANTS.ENV.STAGING });
+
+//   const body = await req.json()
+    console.log(userAlice)
+
+ 
+
+  return NextResponse.json({ output: theResponse }, { status: 200 })
+
+};
