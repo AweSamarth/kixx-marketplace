bb8b5b4885d151cbe2d4a9fe0cb4593e31b4d91e
Harshit GAutam <harshitgtm02@gmail.com>
2023-12-09 15:54:42 +0530

    About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,62 +2,49 @@ import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
-import { styles } from "../styles";
-import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+import { ServiceCard } from "./ServiceCard";
+const services = [
+  {
+    title: "Web Developer",
+    icon: "/backend.png",
+  },
+  {
+    title: "React Native Developer",
+    icon: "/backend.png",
+  },
+  {
+    title: "Backend Developer",
+    icon: "/backend.png",
+  },
+  {
+    title: "Content Creator",
+    icon: "/backend.png",
+  },
+];
 
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
-
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-[70 rem] leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        The Blockchain-Authenticated Sneaker Marketplace is an innovative
+        cutting-edge platform designed to revolutionize the buying and selling
+        of sneakers. This marketplace provides a secure and transparent
+        ecosystem for both established brands and individual resellers to
+        showcase and sell their sneakers with the assurance of authenticity
+        backed by blockchain verification.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
+      <div className="mt-20 flex flex-row flex-wrap gap-10">
+        {services.map((service, index) => {
+          console.log(service); // Log the service object
+
+          return <ServiceCard key={service.title} index={index} {...service} />;
+        })}
       </div>
     </>
   );
