e8bb38fa70f64a72bf676615bb5946bfaff1b116
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-12-09 16:10:59 +0530

    landing page almost done

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,28 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export default function Home() {
   return (<div>
     <ConnectButton />
-    <NavbarNow />
-    <FooterNow />
+    <div className="w-full h-[900px] bg-gradient-to-br from-gray-800 to-black">
+      <NavbarNow />
+      <div className="flex flex-row">
+        <div className="w-[50%] flex flex-col justify-center items-center h-[500px]">
+          <div className="w-[400px] space-y-16">
+            <div className="text-[60px] text-white font-bold tracking-widest">
+              <p className="drop-shadow-lg">KIXX</p>
+              <p className="drop-shadow-lg ">Marketplace</p>
+            </div>
+            <div className="flex flex-row gap-x-16">
+              <button className="border border-white p-4 text-white lg:text-base text-2xl rounded-md">BRANDS</button>
+              <button className="p-4 text-white bg-[#FA662C] lg:text-base text-2xl rounded-md ">RESELLERS</button>
+            </div>
+          </div>
+        </div>
+        {/* For image */}
+        <div className="w-[50%]">
+
+        </div>
+      </div>
     </div>
+    <FooterNow />
+  </div>
   );
 }
